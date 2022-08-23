fd706ce3fd145ddd0d2e243d1f063fc7e5c8e446
Sernasky <besancon.romain8@gmail.com>
2022-08-23 17:11:55 +0200

    Initial commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    /**
+     * @Route("/", name="main_home")
+     */
+    public function home()
+    {
+        return $this->render('main/home.html.twig');
+    }
+
+    /**
+     * @Route("/test", name="main_test")
+     */
+    public function test()
+    {
+        return $this->render('main/test.html.twig');
+    }
+}
