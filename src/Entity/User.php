95e53b7d2ddda9a3d343db7f84a3234751e27668
theomartinsbaltar <theo.martinsbaltar2022@campus-eni.fr>
2022-08-24 15:59:43 +0200

    validationTheo - Validation Mail + champs obligatoires + nom des champs FR

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,11 +5,12 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Il y a déjà un compte avec cette adresse mail')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -18,6 +19,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank(
+        message: 'Ce champ ne devrait pas être vide'
+    )]
+    #[Assert\Email(
+        message: '{{ value }} est une adresse mail invalide.',
+    )]
     private ?string $email = null;
 
     #[ORM\Column]
@@ -30,12 +37,21 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(
+        message: 'Ce champ ne devrait pas être vide'
+    )]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(
+        message: 'Ce champ ne devrait pas être vide'
+    )]
     private ?string $lastname = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(
+        message: 'Ce champ ne devrait pas être vide'
+    )]
     private ?string $telephone = null;
 
     public function getId(): ?int
