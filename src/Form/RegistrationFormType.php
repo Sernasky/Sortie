99c3a9ead152c9b88f85e106f3c4b8d73c5340ec
theomartinsbaltar <theo.martinsbaltar2022@campus-eni.fr>
2022-08-26 14:21:25 +0200

    Maj Merge fin de semaine

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,13 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
