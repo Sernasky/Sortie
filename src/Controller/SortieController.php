aee64c407b9a0f02f76d827a9c6968f22869ab8c
Sernasky <besancon.romain8@gmail.com>
2022-08-30 16:21:36 +0200

    mot de passe oublié et roles /s'inscrire et afficher inscrits

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -15,7 +15,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
+
 
 class SortieController extends AbstractController
 {
